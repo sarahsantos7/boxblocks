f414ad6be1ce4b4d861229ac3da341c152bd70f1
sarah <sarah.dos.santos.7000@gmail.com>
2024-07-13 22:17:53 -0300

    commit

--- js/jogo.js
+++ js/jogo.js
@@ -1,23 +1,42 @@
+var SIZE = 8;
+
 function criaBloco(context, cor, x, y) {
     context.fillStyle = cor;
-    context.fillRect(x, y, 10, 10);
+    context.fillRect(x, y, SIZE, SIZE);
 }
 
-function carregaCenario() {
-    fetch('cenarios/tela1.txt').then(response => { response.text(); })
-        .then(data => {
-            console.log(data);
-        })
+function carregaCenario(context) {
+    fetch('cenarios/tela1.txt').then(response => {
+        return response.text();
+      }).then(cenario => {
+
+        let l = 0;
+        let c = 0;
+        for (let x = 0; x < cenario.length; x++) {
+            let bloco = cenario[x];
+
+            if (bloco == 'G') {
+                criaBloco(context, "#7CFC00", c * SIZE, l * SIZE);
+            } else if (bloco == 'N') {
+                criaBloco(context, "#8B5A2B", c * SIZE, l * SIZE);
+            }
+
+            if (bloco == '\n') {
+                l++;
+                c=0;
+            } else {
+                c++;
+            }
+        }
+      })
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
 
-    carregaCenario();
-
-    criaBloco(context, "#FF0000", 100, 200);
-    criaBloco(context, "#0FFFF0", 10, 20);
+    console.log('1')
+    carregaCenario(context);
 });
 
 
