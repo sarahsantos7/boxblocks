446305f1eb35d15663b67421c65f620692ed322a
sarah <sarah.dos.santos.7000@gmail.com>
2024-07-13 21:09:10 -0300

    commit

--- js/jogo.js
+++ js/jogo.js
@@ -3,9 +3,18 @@ function criaBloco(context, cor, x, y) {
     context.fillRect(x, y, 10, 10);
 }
 
+function carregaCenario() {
+    fetch('cenarios/tela1.txt').then(response => { response.text(); })
+        .then(data => {
+            console.log(data);
+        })
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
+
+    carregaCenario();
 
     criaBloco(context, "#FF0000", 100, 200);
     criaBloco(context, "#0FFFF0", 10, 20);
