9b56ba76ea59bb3e1e35a42d0d9c4ce091fa5d7a
sarah <sarah.dos.santos.7000@gmail.com>
2024-07-28 17:08:05 -0300

    commit

--- js/jogo.js
+++ js/jogo.js
@@ -1,24 +1,43 @@
-var SIZE = 8;
+var TAMANHO_BLOCO = 5;
+var TAMANHO_PIXEL = 7;
+var CONTEXT = null;
+var BLOCOS = [];
 
-function criaBloco(context, cor, x, y) {
-    context.fillStyle = cor;
-    context.fillRect(x, y, SIZE, SIZE);
+carregaBlocos();
+
+function carregaBlocos() {
+    carregaBloco("C");
+    carregaBloco("G");
+    carregaBloco("T");
 }
 
-function carregaCenario(context) {
-    fetch('cenarios/tela1.txt').then(response => {
+function carregaBloco(nome) {
+    fetch('blocos/' + nome +'.txt').then(response => {
+        return response.text();
+    }).then(bloco => {
+      BLOCOS[nome] = bloco;
+    })
+}
+
+
+function achaContext() {
+    return document.getElementById('canvas').getContext('2d');
+}
+
+function carregaCenario(cenario) {
+    CONTEXT.clearRect(0, 0, 1000, 1000)
+    fetch('cenarios/' + cenario + '.txt').then(response => {
         return response.text();
       }).then(cenario => {
 
-        let l = 0;
-        let c = 0;
+        var l = 0;
+        var c = 0;
         for (let x = 0; x < cenario.length; x++) {
-            let bloco = cenario[x];
+            var bloco = cenario[x];
 
-            if (bloco == 'G') {
-                criaBloco(context, "#7CFC00", c * SIZE, l * SIZE);
-            } else if (bloco == 'N') {
-                criaBloco(context, "#8B5A2B", c * SIZE, l * SIZE);
+            if (bloco != '\n' && bloco != ' ') {
+                //console.log(BLOCOS)
+                desenhaBloco(BLOCOS[bloco], c, l);
             }
 
             if (bloco == '\n') {
@@ -31,13 +50,40 @@ function carregaCenario(context) {
       })
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
+function desenhaBloco(bloco, x, y) {
+    var l = 0;
+    var c = 0;
+    for (var p = 0; p < bloco.length; p++) {
+        var pixel = bloco[p];
 
-    console.log('1')
-    carregaCenario(context);
+        var pixelX = (x * TAMANHO_BLOCO * TAMANHO_PIXEL) + (c * TAMANHO_PIXEL);
+        var pixelY = (y * TAMANHO_BLOCO * TAMANHO_PIXEL) + (l * TAMANHO_PIXEL);
+
+        if (pixel == 'G') {
+            desenhaPixel("#7CFC00", pixelX, pixelY);
+        } else if (pixel == 'N') {
+            desenhaPixel("#8B5A2B", pixelX, pixelY);
+        }
+
+        if (pixel == '\n') {
+            l++;
+            c=0;
+        } else {
+            c++;
+        }
+    }
+}
+
+function desenhaPixel(cor, x, y) {
+    CONTEXT.fillStyle = cor;
+    CONTEXT.fillRect(x, y, TAMANHO_PIXEL, TAMANHO_PIXEL);
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    CONTEXT = achaContext();
+    setTimeout(carregaCenario('tela1'), 200);
 });
+
 
 
 
