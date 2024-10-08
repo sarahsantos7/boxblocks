1fd768b0856e51f55a20f6bf8f72f27685bb2284
Rafael Topera <rtopera@iriusrisk.com>
2024-10-08 17:12:08 -0300

    Criando classe Jogo

--- js/classes/Jogo.js
+++ js/classes/Jogo.js
@@ -1,5 +1,16 @@
+import Carregador from './classes/Carregador.js'
 
 class Jogo {
+
+    constructor(){
+        this.blocos = []
+    }
+
+    inicia(){
+        let carregador = new Carregador()
+        //blocos = await carregador.carregaBlocos()
+        comecaJogo()
+    }
 
 }
 
